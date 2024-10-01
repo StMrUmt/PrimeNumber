1053341e3ceccb8c43db2b2bef98501075745ec3
Umut <Umut@UMUTBUSINESS>
2024-10-01 14:31:46 +0300

    Add project files.

---
+++ ConsoleAppPrimeNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static bool IsItaPrimeNumber(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3 Numbers");
+
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int number3 = Convert.ToInt32(Console.ReadLine());
+
+            int total = number1 + number2 + number3;
+
+            if (IsItaPrimeNumber(total))
+                Console.WriteLine("Is a prime number");
+
+            else
+            {
+                Console.WriteLine("Is not a prime number");
+            };
+            Console.ReadLine();
+        }
+
+    }
+}
+// Hello, the purpose of the "Math.Sqrt" code here is to return the square root of the specified number.
